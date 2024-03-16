6419d217cf3694e95ed4a489e6f84bf2a14ebec5
speer987 <saarah.peer15@gmail.com>
2024-03-16 15:36:05 -0700

    updated linkedin profile link to see if it works

--- src/components/ContactBlock.js
+++ src/components/ContactBlock.js
@@ -1,7 +1,7 @@
 export default function ContactBlock() {
   return (
     <div id="contact">
-      <a href="https://linkedin.com/saarah-peer" target="_blank">
+      <a href="www.linkedin.com/in/saarah-peer" target="_blank">
         <button id="linkedin">
           <i class="fa fa-linkedin"></i>
         </button>
